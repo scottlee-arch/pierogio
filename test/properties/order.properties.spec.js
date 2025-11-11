cfdb867b06bfdb500718e176154185e0cc5f802f
scottlee-arch <scottlee@andrew.cmu.edu>
2025-11-11 15:14:05 +0000

    Add a property test for tax()

--- test/properties/order.properties.spec.js
+++ test/properties/order.properties.spec.js
@@ -66,6 +66,17 @@ describe('Property-Based Tests for Orders', () => {
       );
     });
 
+    it('tax should always be non-negative integer', () => {
+        fc.assert(
+        fc.property(orderArb, (order) => {
+          const result = tax(order, {});
+          return result >= 0 && Number.isInteger(result);
+        }),
+        { numRuns: 50 }
+      );
+    });
+
+
     // ---------------------------------------------------------------------------
     // Add more invariant properties for discounts, tax, delivery, and total here
     // You can adapt the starter code below.
